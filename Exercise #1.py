cdd636f9cb9423a52fe8fa7ee91db91231cfe23b
Rasuljon86 <82506917+Rasuljon86@users.noreply.github.com>
2021-05-01 14:13:53 +0500

    Add files via upload

---
+++ Exercise #1.py
@@ -0,0 +1,8 @@
+# Hello Admin
+usernames = ["Jasur", "Hamid", "admin", "Erkin", 'ever']
+
+for username in usernames:
+    if username == 'admin':
+        print("Hello admin!")
+    else:
+        print("Hello " + username + ", thank you for logging in again!")
